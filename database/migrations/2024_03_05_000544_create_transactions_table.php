98a4c688a29c25dbebd3dd25a34a7dcadfdb86f1
abghifareihand <info.abghifareihan@gmail.com>
2024-03-05 00:23:06 +0700

    update

--- database/migrations/2024_03_05_000544_create_transactions_table.php
+++ database/migrations/2024_03_05_000544_create_transactions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_id');
+            $table->string('transaction_number');
             $table->text('address')->nullable();
             $table->float('shipping_price')->default(0);
             $table->float('total_price')->default(0);
